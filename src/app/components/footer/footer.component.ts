2c3717f2d3949232b49b0b31312337cb2c997129
Vandenkerkhof vincent <chouvdj@gmail.com>
2025-10-05 00:31:38 +0200

    footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+// src/app/components/footer/footer.component.ts
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+  version = '1.0.0';
+}
